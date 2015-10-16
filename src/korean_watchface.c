598458672c01a6cad3455378bde767180890e686
David Wen <dstwen@gmail.com>
2015-10-16 14:11:18 -0700

    Remove unnecessary comments; destroy bitmap and layer

--- src/korean_watchface.c
+++ src/korean_watchface.c
@@ -1,7 +1,6 @@
 #include <pebble.h>
   
 static Window *s_main_window;
-static TextLayer *s_time_layer;
 
 static GBitmap *s_hour_1_bitmap;
 static BitmapLayer *s_hour_1_layer;
@@ -22,9 +21,6 @@ static void update_time() {
     //Use 12 hour format
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
-
-  // Display this time on the TextLayer
-  // text_layer_set_text(s_time_layer, buffer);
 }
 
 static void main_window_load(Window *window) {
@@ -35,27 +31,17 @@ static void main_window_load(Window *window) {
   s_hour_1_layer = bitmap_layer_create(GRect(6, 0, 33, 33));
   bitmap_layer_set_bitmap(s_hour_1_layer, s_hour_1_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_hour_1_layer));
-
-  // // Create time TextLayer
-  // s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
-  // text_layer_set_background_color(s_time_layer, GColorClear);
-  // text_layer_set_text_color(s_time_layer, GColorBlack);
-  // text_layer_set_text(s_time_layer, "00:00");
-
-  // // Improve the layout to be more like a watchface
-  // text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  // text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
-  // // Add it as a child layer to the Window's root layer
-  // layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   
   // Make sure the time is displayed from the start
   update_time();
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy TextLayer
-  text_layer_destroy(s_time_layer);
+  // Destroy GBitmap
+  gbitmap_destroy(s_hour_1_bitmap);
+
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_hour_1_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
